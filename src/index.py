408657aea0a505601850c4d3b80fb6f8b991e16c
sabmeua <sabme.ua@gmail.com>
2020-07-13 16:34:07 +0900

    Use AWS IoT certificates to upstream videos to KVS

--- src/index.py
+++ src/index.py
@@ -14,10 +14,22 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
+STREAM_NAME = os.environ.get('STREAM_NAME')
+IOT_CREDENTIAL_ENDPOINT = os.environ.get('IOT_CREDENTIAL_ENDPOINT')
+CERT_ID = os.environ.get('CERT_ID')
+
+KVS_CMD = f'kvssink stream-name={STREAM_NAME} framerate=15'\
+          ' aws-region=ap-northeast-1'\
+          ' iot-certificate="iot-certificate,'\
+          f'endpoint={IOT_CREDENTIAL_ENDPOINT},'\
+          f'cert-path=/greengrass/certs/{CERT_ID}.cert.pem,'\
+          f'key-path=/greengrass/certs/{CERT_ID}.private.key,'\
+          'ca-path=/greengrass/certs/root.ca.pem,'\
+          'role-aliases=KvsCameraIoTRoleAlias"'
+
 DEFAULT_PIPELINE = 'videotestsrc ! clockoverlay auto-resize=false'\
                    ' ! videorate ! video/x-raw,format=I420,framerate=15/1'\
-                   ' ! x264enc tune=zerolatency ! h264parse'\
-                   ' ! kvssink stream-name=test framerate=15'
+                   ' ! x264enc tune=zerolatency ! h264parse'
 
 def on_message(bus: Gst.Bus, message: Gst.Message, loop: GLib.MainLoop):
     mtype = message.type
@@ -41,7 +53,12 @@ def main():
 
     Gst.init(sys.argv)
 
-    pipeline = Gst.parse_launch(os.environ.get('PIPELINE', DEFAULT_PIPELINE))
+    cmd = os.environ.get('PIPELINE', DEFAULT_PIPELINE)
+    if os.environ.get('USE_KVS', True):
+        cmd += f' ! {KVS_CMD}'
+    logger.info('Pipeline : %s', cmd)
+
+    pipeline = Gst.parse_launch(cmd)
     bus = pipeline.get_bus()
     bus.add_signal_watch()
     pipeline.set_state(Gst.State.PLAYING)
